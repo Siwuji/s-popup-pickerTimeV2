73c8498c3c1bfffeb5e99d2268ff55f74a746fa8
思无及 <50439181+Siwuji@users.noreply.github.com>
2023-06-19 09:51:48 +0800

    Add files via upload

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,32 +1,36 @@
 <template>
 	<view class="content">
 		<view>当前日期时间格式为:{{dateOption.format}}</view>
-		<input @focus="showPicker" placeholder="默认格式yyyy-MM-dd" :value="value" />
-		<button @click="changeDateParms">年月日时分秒</button>
-		<button @click="changeDateParms1">年月日时分?</button>
-		<button @click="changeDateParms2">来个中文?</button>
+		<input @focus="showPicker" placeholder="默认格式yyyy-MM-dd" :value="dateOption.defaultValue" />
+		<button @click="changeDateParms('yyyy-MM-dd HH:mm:ss')">年月日时分秒</button>
+		<button @click="changeDateParms('yyyy-MM-dd HH:mm')">年月日时分?</button>
+		<button @click="changeDateParms('MM-dd HH:mm')">月日时分</button>
+		<button @click="changeDateParms('HH:mm:ss')">时分秒</button>
+		<button @click="changeDateParms(('yyyy年MM月dd日 HH时mm分ss秒'))">来个中文?</button>
 
 		<view class="default">
 			注释：<br />
 			1.format默认为yyyy-MM-dd，可不传。<br />
 			2.能通过uni编译的小程序应该都可以使用,因我只试了微信和钉钉，所以其它小程序需要自行测试。<br />
+			3.defaultValue/minTime/maxTime尽量和format格式相同<br />
+			4.从时分秒转换为年月日时,记得将minTime/maxTime或者yearArea同步修改一下，否则可能切换后年份只有当年<br />
+			5.如果minTime/maxTime有值，优先级高于yearArea<br />
 		</view>
 		<sDatePicker ref="datePicker" @confirm="saveDate" :option="dateOption"></sDatePicker>
 	</view>
 </template>
 
 <script>
-	import sDatePicker from '@/components/s-popup-pickerTime/s-popup-pickerTime.vue';
+	import sDatePicker from '@/components/swj-popup-pickerTime/swj-popup-pickerTime.vue';
 	export default {
 		data() {
 			return {
-				value: "2022-05-10",
 				dateOption: {
 					format: 'yyyy-MM-dd', //日期格式
-					defaultValue: "2022-05-10", //非必填：默认时间
-					minTime: '2022-01-11 05:12:01', //非必填:最小时间
-					maxTime: '2023-06-02 12:23:30', //非必填:最大时间
-					yearArea: [2020, 2023], //非必填:年份区间，如果minTime/maxTime有值，优先级高于yearArea
+					defaultValue: "2022-05-06", //非必填：默认时间
+					minTime: '', //非必填:最小时间
+					maxTime: '', //非必填:最大时间
+					yearArea: [2021,2025], //非必填:年份区间[2021,2025]，如果minTime/maxTime有值，优先级高于yearArea
 					showClearBtn: true //是否显示清除按钮
 				}
 			}
@@ -45,16 +49,9 @@
 				//日期选择器【确认】事件
 				// console.log(e)
 				this.dateOption.defaultValue = e;
-				this.value = e;
 			},
-			changeDateParms() {
-				this.dateOption.format = "yyyy-MM-dd HH:mm:ss"
-			},
-			changeDateParms1() {
-				this.dateOption.format = "yyyy-MM-dd HH:mm"
-			},
-			changeDateParms2() {
-				this.dateOption.format = "yyyy年MM月dd日HH时mm分ss秒"
+			changeDateParms(format) {
+				this.dateOption.format = format
 			}
 		}
 	}
